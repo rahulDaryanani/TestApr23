ad18c7f4ff83eda4ebdfab2ab7459b105607550a
Rahul Daryanani <rahul.daryanani32@gmail.com>
2023-05-21 12:37:20 +0530

    adding package A

---
+++ src/A/File1.java
@@ -0,0 +1,12 @@
+package A;
+
+public class File1 {
+
+	public static void main(String[] args) {
+		System.out.println("in branch A");
+		System.out.println("a");
+		
+		System.out.println("B");
+	}
+
+}
