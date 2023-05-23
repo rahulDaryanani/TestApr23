4569c27b67df2a61249ae3b7a5e98e1f6fe3a090
Rahul Daryanani <rahul.daryanani32@gmail.com>
2023-05-23 08:18:46 +0530

    Commit in B2 for conflict

--- src/basicConcepts/StringPractice.java
+++ src/basicConcepts/StringPractice.java
@@ -4,5 +4,7 @@ public class StringPractice {
 
 	public static void main(String[] args) {
 		System.out.println("in main method");
+		System.out.println("Test in B1");
+		System.out.println("creating conflict");
 	}
 }
